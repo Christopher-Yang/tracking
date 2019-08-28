3283336b33120faf453c838f84f52910c4d36a9d
Chris <Shimohara4@gmail.com>
2019-08-28 14:03:09 -0400

    First commit of branch. Changed code to correct frequency-domain averaging in analyze_data.m

--- main.m
+++ main.m
@@ -1,14 +1,10 @@
 clear all;
 
-folder = 'Data/bimanual_pilot/';
+folder = 'Data/baseline_learning/';
 time = 60; %in seconds
-theta = 90;
-% block_name = {'B1','B2','B8','B21','B22_habit'};
-% graph_name = {'Baseline','Early','Middle','Late','Habit'};
-% subj_name = {'subj1','subj2','subj3'};
-block_name = {'B1_baseline','B11','B14','B17','B20','B23'};
-graph_name = {'B1','B11','B14','B17','B20','B23'};
-subj_name = {'subj5','subj6'};
+block_name = {'left1','left4','right1','right4'};
+graph_name = {'Left Early','Left Late','Right Early','Right Late'};
+subj_name = {'subj1'};
 d = load_data(subj_name,block_name,folder,time);
 data = analyze_data(d,block_name,0);
 
@@ -24,12 +20,12 @@ gblocks = [2:5];
 graph_bode_simple(data, graph_name, gblocks);
 
 %% graph performance
-gblocks = 1:6;
+gblocks = 1:4;
 
 % graph_amp_avg(data,block_name,gblocks,graph_name); % amplitude spectrums
-graph_MSE(data, block_name, graph_name); % mean squared error
+% graph_MSE(data, block_name, graph_name); % mean squared error
 % graph_rotatedMSE(data, block_name, gblocks, graph_name);
 % graph_angle(data, gblocks,theta); % angle of movement error
-% graph_lag(data, gblocks, graph_name); % response lag
+graph_lag(data, gblocks, graph_name,'cursor'); % response lag
 % graph_complexError(data, graph_name, gblocks); % complex tracking error
 % graph_coherence(data, gblocks, graph_name) % coherence
