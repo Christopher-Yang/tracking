6029fccc4c1bec04e81b45d683dd1b6d2eff86cf
Chris <Shimohara4@gmail.com>
2019-12-02 14:07:21 -0500

    Made minor changes to plotting, can't remember what they are

--- phasor_1d.m
+++ phasor_1d.m
@@ -1,10 +1,12 @@
-subj = 1;
+function phasor_1d(data)
+
+subj = 2;
 
 figure(1); clf
 subplot(1,2,1); hold on
 plot([-2 2],[0 0],'k','LineWidth',1)
 plot([0 0],[-2 2],'k','LineWidth',1)
-plot(permute(data{subj}.org.phasors.cursor.x_x_all.ratio,[2 1]),'.','MarkerSize',20)
+plot(permute(data.org.phasors.cursor.x_x_all.ratio,[2 1]),'.','MarkerSize',20)
 axis([-1 1 -1 1])
 pbaspect([1 1 1])
 title('Original mapping')
@@ -12,7 +14,7 @@ title('Original mapping')
 subplot(1,2,2); hold on
 plot([-2 2],[0 0],'k','LineWidth',1)
 plot([0 0],[-2 2],'k','LineWidth',1)
-plot(permute(data{subj}.new.phasors.cursor.x_x_all.ratio,[2 1]),'.','MarkerSize',20)
+plot(permute(data.new.phasors.cursor.x_x_all.ratio,[2 1]),'.','MarkerSize',20)
 axis([-1 1 -1 1])
 pbaspect([1 1 1])
 title('New mapping')
