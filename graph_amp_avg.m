9ce7acedfaf7f02ecb2cf988686f40add4b5cb1b
Chris <Shimohara4@gmail.com>
2019-12-05 18:10:39 -0500

    graph_amp_avg.m now works

--- graph_amp_avg.m
+++ graph_amp_avg.m
@@ -1,13 +1,8 @@
-function graph_amp_avg(data,block_name,gblocks,graph_name)
+function graph_amp_avg(data,block_name,gblocks,graph_name,output)
     
-    col = [255 69 0
-           153 50 204
+    col = [30 144 255
            0 0 139
-           255 182 193
-           199 178 153
-           128 0 0]./255;
-    fontSize = 12;
-    titleSize = 14;
+           255 182 193]./255;
     lw = 1;
     
     H = gobjects(length(gblocks),1);
@@ -15,18 +10,15 @@ function graph_amp_avg(data,block_name,gblocks,graph_name)
         H(i) = figure;
     end
     
-%     d = data;
-%     subj = fieldnames(d);
-%     subj(end) = [];
     Nsubj = length(data)-1;
-    freqsX = data{end}.x_x.freqs;
-    freqsY = data{end}.y_y.freqs;
-    ampX = data{end}.x_x.amp;
-    ampY = data{end}.y_y.amp;
-    xAxis = data{end}.x_x.x_axis;
-    ix = data{end}.x_x.index;
-    iy = data{end}.y_y.index;
-    Nsteps = length(data{1}.(block_name{1}).cursor.x_pos);
+    freqsX = data{end}.freqX;
+    freqsY = data{end}.freqY;
+    ampX = data{end}.ampX;
+    ampY = data{end}.ampY;
+    xAxis = data{end}.x_axis;
+    ix = data{end}.(output).x_x.index;
+    iy = data{end}.(output).y_y.index;
+    Nsteps = length(data{1}.(block_name{1}).(output).x_pos);
     
     g1 = NaN(length(xAxis),length(gblocks),Nsubj);
     g2 = NaN(length(xAxis),1000,length(gblocks));
@@ -41,8 +33,11 @@ function graph_amp_avg(data,block_name,gblocks,graph_name)
     
     for i = 1:Nsubj
         for j = 1:length(gblocks)
-            a.x_all(:,j,i) = data{i}.(block_name{gblocks(j)}).cursor.x_fft.amplitude; % puts each subject's amplitude spectrums into data structure
-            a.y_all(:,j,i) = data{i}.(block_name{gblocks(j)}).cursor.y_fft.amplitude;
+            dat = data{i}.(block_name{gblocks(j)});
+            pX = dat.(output).x_fft;
+            pY = dat.(output).y_fft;
+            a.x_all(:,j,i) = squeeze(abs(mean(pX,2)));
+            a.y_all(:,j,i) = squeeze(abs(mean(pY,2)));
         end
     end
     
@@ -65,61 +60,37 @@ function graph_amp_avg(data,block_name,gblocks,graph_name)
     
     for i = 1:length(gblocks)
         figure(H(i))
-        subplot(2,1,1)
-        plot(freqsX,ampX,'o','Color',col(1,:),'LineWidth',lw,'MarkerFaceColor',col(1,:),'MarkerEdgeColor','none')
-        hold on
-        plot(freqsY,ampY,'o','Color',col(2,:),'LineWidth',lw,'MarkerFaceColor',col(2,:),'MarkerEdgeColor','none')
-        plot([50 51], [50 51],'Color',col(3,:),'LineWidth',lw)
-        plot([50 51], [50 51],'--','Color',col(5,:),'LineWidth',lw)
-        if i == 1
-            % plot mean with error bars; doesn't work if amplitude
-            % spectra are averaged in the complex domain
-            %                 s = shadedErrorBar(xAxis, a.x(:,i),a.errx(:,:,i));
-            %                 editErrorBar(s,col(3,:),0.25);
-            plot(xAxis,a.x(:,i),'Color',col(3,:),'LineWidth',lw)
-            plot(xAxis,a.x_all(:,:,i),'Color',[col(3,:) 0.15],'LineWidth',0.25)
-            plot(freqsX,a.x(ix,i),'o','Color',col(1,:),'LineWidth',lw,'MarkerFaceColor',col(1,:),'MarkerEdgeColor','none')
-        else
-            %                 s = shadedErrorBar(xAxis, a.y(:,i),a.erry(:,:,i));
-            %                 editErrorBar(s,col(3,:),0.25);
-            plot(xAxis,a.y(:,i),'Color',col(3,:),'LineWidth',lw)
-            plot(xAxis,a.y_all(:,:,i),'Color',[col(3,:) 0.15],'LineWidth',0.25)
-            plot(xAxis,a.x(:,1),'--','Color',col(5,:),'LineWidth',lw)
-            plot(freqsY,a.y(iy,i),'o','Color',col(2,:),'LineWidth',lw,'MarkerFaceColor',col(2,:),'MarkerEdgeColor','none')
-        end
-        set(gca,'Xtick',0:1:2,'Ytick',0:0.01:0.03,'box','off','LineWidth',1,'TickDir','out','FontSize',fontSize,'XMinorTick','on')
+        subplot(2,1,1); hold on
+        plot(freqsX,ampX,'ko','LineWidth',lw,'MarkerFaceColor',[0 0 0])
+        plot(freqsY,ampY,'ko','LineWidth',lw)
+        % plot mean with error bars; doesn't work if amplitude spectra are averaged in the complex domain
+%         s = shadedErrorBar(xAxis, a.x(:,i),a.errx(:,:,i));
+%         editErrorBar(s,col(3,:),0.25);
+        plot(xAxis,a.x(:,i),'Color',col(1,:),'LineWidth',lw)
+        plot(freqsX,a.x(ix,i),'ko-','LineWidth',lw,'MarkerFaceColor',[0 0 0])
+        plot(freqsY,a.x(iy,i),'ko-','LineWidth',lw)
+        set(gca,'Xtick',0:1:2,'Ytick',0:0.01:0.03,'box','off','LineWidth',1,'TickDir','out','XMinorTick','on')
         ax = gca;
         ax.XAxis.MinorTickValues = 0:0.25:2.25;
-        axis([0 2.3 0 ampX(1)+.01])
+        axis([0 2.3 0 0.025])
         ylabel('X Amplitude (m)')
-        title(graph_name{gblocks(i)},'FontSize',titleSize)
-        legend({'X target','Y target','Hand','Baseline'})
+        title(graph_name{gblocks(i)})
+        legend({'X target','Y target','Hand'})
         legend boxoff
         
-        subplot(2,1,2)
-        plot(freqsX, ampX,'o','LineWidth',lw,'Color',col(1,:),'MarkerFaceColor',col(1,:),'MarkerEdgeColor','none')
-        hold on
-        plot(freqsY, ampY,'o','LineWidth',lw,'Color',col(2,:),'MarkerFaceColor',col(2,:),'MarkerEdgeColor','none')
-        if i == 1
-            %                 s = shadedErrorBar(xAxis, a.y(:,i),a.erry(:,:,i),'lineProps','-b');
-            %                 editErrorBar(s,col(3,:),0.25);
-            plot(xAxis,a.y(:,i),'LineWidth',lw,'Color',col(3,:),'MarkerFaceColor',col(3,:))
-            plot(xAxis,a.y_all(:,:,i),'Color',[col(3,:) 0.15],'LineWidth',0.25)
-            plot(freqsY,a.y(iy,i),'o','LineWidth',1.5,'Color',col(2,:),'MarkerFaceColor',col(2,:),'MarkerEdgeColor','none')
-        else
-            %                 s = shadedErrorBar(xAxis, a.x(:,i),a.errx(:,:,i),'lineProps','-b');
-            %                 editErrorBar(s,col(3,:),0.25);
-            plot(xAxis,a.x(:,i),'LineWidth',lw,'Color',col(3,:),'MarkerFaceColor',col(3,:))
-            plot(xAxis,a.x_all(:,:,i),'Color',[col(3,:) 0.15],'LineWidth',0.25)
-            plot(xAxis,a.y(:,1),'--','Color',col(5,:),'LineWidth',lw)
-            plot(freqsX,a.x(ix,i),'o','LineWidth',lw,'Color',col(1,:),'MarkerFaceColor',col(1,:),'MarkerEdgeColor','none')
-        end
-        set(gca,'Xtick',0:1:2,'Ytick',0:0.01:0.03,'box','off','LineWidth',1,'TickDir','out','FontSize',fontSize,'XMinorTick','on')
+        subplot(2,1,2); hold on
+        plot(freqsX, ampX,'ko','LineWidth',lw,'MarkerFaceColor',[0 0 0])
+        plot(freqsY, ampY,'ko','LineWidth',lw)
+%         s = shadedErrorBar(xAxis, a.y(:,i),a.erry(:,:,i),'lineProps','-b');
+%         editErrorBar(s,col(3,:),0.25);
+        plot(xAxis,a.y(:,i),'LineWidth',lw,'Color',col(1,:))
+        plot(freqsY,a.y(iy,i),'ko-','LineWidth',lw)
+        plot(freqsX,a.y(ix,i),'ko-','LineWidth',lw,'MarkerFaceColor',[0 0 0])
+        set(gca,'Xtick',0:1:2,'Ytick',0:0.01:0.03,'box','off','LineWidth',1,'TickDir','out','XMinorTick','on')
         ax = gca;
         ax.XAxis.MinorTickValues = 0:0.25:2.25;
-        axis([0 2.3 0 ampY(1)+.01])
+        axis([0 2.3 0 0.025])
         ylabel('Y Amplitude (m)')
         xlabel('Frequency (Hz)')
-        %             pbaspect([1 1 1])
     end
 end
--- main.m
+++ main.m
@@ -10,10 +10,10 @@ clear all;
 
 folder = 'Data/baseline_learning/';
 time = 60; %in seconds
-% block_name = {'right1','right2','right3','right4','right5'};
-% graph_name = {'Right 1','Right 2','Right 3','Right 4','Right 5'};
-block_name = {'right1','F1_1','F3_1','F4_1'};
-graph_name = block_name;
+block_name = {'right1','right2','right3','right4','right5'};
+graph_name = {'Right 1','Right 2','Right 3','Right 4','Right 5'};
+% block_name = {'right1','F1_1','F3_1','F4_1'};
+% graph_name = block_name;
 subj_name = {'subj3'};
 d = load_data(subj_name,block_name,folder,time);
 data = analyze_data(d,block_name,0,0);
@@ -30,12 +30,13 @@ gblocks = [2:5];
 graph_bode_simple(data, graph_name, gblocks);
 
 %% graph performance
-gblocks = 1:4;
+gblocks = 1;
+output = 'Rhand';
 
-% graph_amp_avg(data,block_name,gblocks,graph_name); % amplitude spectrums
+graph_amp_avg(data,block_name,gblocks,graph_name,output); % amplitude spectrums
 % graph_MSE(data, block_name, graph_name); % mean squared error
 % graph_rotatedMSE(data, block_name, gblocks, graph_name);
 % graph_angle(data, gblocks,theta); % angle of movement error
-graph_lag(data, gblocks, graph_name,'cursor'); % response lag
+% graph_lag(data, gblocks, graph_name,'cursor'); % response lag
 % graph_complexError(data, graph_name, gblocks); % complex tracking error
 % graph_coherence(data, gblocks, graph_name) % coherence
